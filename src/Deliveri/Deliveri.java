e3c7d2275e310753476868486c836bea1f73f0a9
jalmazbekov <hazim.almazbekov@gmail.com>
2026-01-21 20:45:05 +0600

    вы решили конфликт

---
+++ src/Deliveri/Deliveri.java
@@ -0,0 +1,11 @@
+package Deliveri;
+
+public class Deliveri
+{
+    public static void main(String[] args) {
+
+        System.out.println("dsfg");
+        System.out.println("яндекс");
+        System.out.println("глово");
+    }
+}
